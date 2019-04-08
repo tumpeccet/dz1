75357ee6f0399257748ee4a0831c5df1652fa386
five <five@josips-macbook-pro.local>
2019-04-08 17:11:51 +0200

    Initial Commit

---
+++ dz1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dz1
+//
+//  Created by five on 4/8/19.
+//  Copyright © 2019 five. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
